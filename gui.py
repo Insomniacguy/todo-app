e5fbea1849e53423437853340606bd7b98cd1e7d
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-01 23:21:36 +0530

    Implemented the Edit functionality

--- gui.py
+++ gui.py
@@ -6,9 +6,10 @@ import PySimpleGUI as Sg
 label = Sg.Text("Type in a to-do")
 input_box = Sg.Input(tooltip="Enter a todo", key='todo')
 button = Sg.Button("Add")
-
+list_box = Sg.Listbox(values=functions.get_todos(), size=(50, 10), key='lb', enable_events=True)
+edit_button = Sg.Button("Edit")
 window = Sg.Window('To-do App',
-                   layout=[[label], [input_box, button]],
+                   layout=[[label], [input_box, button], [list_box, edit_button]],
                    font=('Helvetica', 20))  # layout arg requires a list
 
 while True:
@@ -24,7 +25,21 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
-        case Sg.WIN_CLOSED: # variable defined in PySimpleGUI module
+            list_box.update(values=todos)
+        case "Edit":
+            edited_todo = values['lb'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            # print(type(todos))
+            # print(todos)
+            # index = todos.index(edited_todo)
+            index = todos.index(edited_todo)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            list_box.update(values=todos)
+        case "lb":
+            input_box.update(value=values['lb'][0])
+        case Sg.WIN_CLOSED:  # variable defined in PySimpleGUI module
             break
 
 window.close()  # close is a third-party method of third party type/class Window of third party library PySimpleGUI
