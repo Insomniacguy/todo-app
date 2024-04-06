84a2ddc0787f77374bd89001fee213e3d3cfcc01
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-06 20:30:45 +0530

    Handle complete and edit errors, change theme, add clock

--- gui.py
+++ gui.py
@@ -2,7 +2,10 @@
 
 import functions
 import PySimpleGUI as Sg
+import time
 
+Sg.theme('Black')
+clock = Sg.Text('', key='clock')
 label = Sg.Text("Type in a to-do")
 input_box = Sg.Input(tooltip="Enter a todo", key='todo')
 button = Sg.Button("Add")
@@ -12,19 +15,21 @@ complete_button = Sg.Button("Complete")
 # layout = [[label], [input_box, button], [list_box, edit_button]]  # Dynamically creating layout
 exit_button = Sg.Button("Exit")
 window = Sg.Window('To-do App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=('Helvetica', 20))  # layout arg requires a list
 
 while True:
-    event, values = window.read()  # read is a third-party method of third party type/class Window of third party
+    event, values = window.read(timeout=200)  # read is a third-party method of third party type/class Window of third party
     # library PySimpleGUI
     print(event)
     # print(type(event))  # read() returns a tuple
     print(values)
     # print(type(values))
+    window["clock"].update(value=time.strftime("%b %d, %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -33,23 +38,31 @@ while True:
             functions.write_todos(todos)
             list_box.update(values=todos)
         case "Edit":
-            edited_todo = values['lb'][0]
-            new_todo = values['todo'] + "\n"
-            todos = functions.get_todos()
-            # print(type(todos))
-            # print(todos)
-            # index = todos.index(edited_todo)
-            index = todos.index(edited_todo)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            list_box.update(values=todos)
+            try:
+                edited_todo = values['lb'][0]
+                new_todo = values['todo'] + "\n"
+                todos = functions.get_todos()
+                # print(type(todos))
+                # print(todos)
+                # index = todos.index(edited_todo)
+                index = todos.index(edited_todo)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                list_box.update(values=todos)
+            except IndexError:
+                Sg.popup('please select an item first', font='Helvetica 20')
+
         case 'Complete':
-            completed_todo = values['lb'][0]
-            todos = functions.get_todos()
-            deleted_todo = todos.remove(completed_todo)
-            functions.write_todos(todos)
-            window['lb'].update(values=todos)
-            input_box.update(value='')
+            try:
+                completed_todo = values['lb'][0]
+                todos = functions.get_todos()
+                deleted_todo = todos.remove(completed_todo)
+                functions.write_todos(todos)
+                window['lb'].update(values=todos)
+                input_box.update(value='')
+            except IndexError:
+                Sg.popup('please select an item first', font='Helvetica 20')
+
         case "lb":
             input_box.update(value=values['lb'][0])
             # value is an arg of elements such as buttons, input box in PySimpleGUI.
